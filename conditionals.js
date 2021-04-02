2a98c2a261c08a7f847b53df369035cd8cdd5006
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-01 17:58:06 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -5,6 +5,10 @@ if (raining) {
   console.log("Don't forget your umbrella!");
 }
 
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
 if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
@@ -21,4 +25,15 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
